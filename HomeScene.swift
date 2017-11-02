0bd7f451e2087b668c889ca07448b66b54c45e67
sallzzbr <evio.salgado@hotmail.com>
2017-11-02 09:29:10 -0200

    bandeira e pig animados adicionados na home

--- HomeScene.swift
+++ HomeScene.swift
@@ -18,9 +18,11 @@ class HomeScene: SKScene {
     var velocityX:CGFloat = 0.0
     
     
-        var pigComendo = [SKTexture]()
+    var pigComendo = [SKTexture]()
+    var bandeiraGrande = [SKTexture]()
     //    var pigAndando = [SKTexture]()
-        var pig = SKSpriteNode()
+    var pig = SKSpriteNode()
+    var bandeirao = SKSpriteNode()
     //    var pig2 = SKSpriteNode()
     
     override func sceneDidLoad() {
@@ -70,25 +72,33 @@ class HomeScene: SKScene {
         
         //
         //
-                for i in 1...2 {
-                    pigComendo.append(SKTexture(imageNamed:("porquinho-comendo\(i)")))
-                }
+        for i in 1...2 {
+            pigComendo.append(SKTexture(imageNamed:("porquinho-comendo\(i)")))
+        }
         //
         //        for i in 1...2 {
         //            pigAndando.append(SKTexture(imageNamed:("pigwalk\(i)")))
         //        }
         //
-                pig = SKSpriteNode(texture: pigComendo[0])
-                pig.zPosition = 2
-                pig.setScale(0.14)
+        for i in 1...6 {
+            bandeiraGrande.append(SKTexture(imageNamed:("Bandeira1frame\(i)")))
+        }
+        
+        pig = SKSpriteNode(texture: pigComendo[0])
+        pig.zPosition = 2
+        pig.setScale(0.14)
         //
         //        pig2 = SKSpriteNode(texture: pigComendo[0])
         //        pig2.zPosition = 2
         //        pig2.setScale(0.14)
         //
         //
+        bandeirao = SKSpriteNode(texture: bandeiraGrande[0])
+        bandeirao.zPosition = 2
+        bandeirao.setScale(0.21)
         //
-                let comendoAction = SKAction.animate(with: pigComendo, timePerFrame: 0.9, resize: true, restore: false)
+        let comendoAction = SKAction.animate(with: pigComendo, timePerFrame: 0.9, resize: true, restore: false)
+        let balancarAction = SKAction.animate(with: bandeiraGrande, timePerFrame: 0.02, resize: true, restore: false)
         //
         //
         //        let andandoAction = SKAction.animate(with: pigAndando, timePerFrame: 0.5, resize: true, restore: false)
@@ -102,14 +112,21 @@ class HomeScene: SKScene {
         //
         //         let group = SKAction.group([andandoAction,direita])
         //
-                pig.position = CGPoint(x: frame.midX - 30, y: frame.midY - 139)
+        pig.position = CGPoint(x: frame.midX - 30, y: frame.midY - 155)
         //        pig2.position = CGPoint(x: frame.midX + 100, y: frame.midY - 139)
         //
-                let repeatAction = SKAction.repeatForever(comendoAction)
+        bandeirao.position = CGPoint(x: frame.midX - 120, y: frame.midY - 115)
+        
+        let repeatAction = SKAction.repeatForever(comendoAction)
+        
+        let repeatBandeira = SKAction.repeatForever(balancarAction)
         //     //   let repeatAndando = SKAction.repeat(group, count: 3)
         //
-                pig.run(repeatAction, withKey: "repeatAction")
-                addChild(pig)
+        pig.run(repeatAction, withKey: "repeatAction")
+        addChild(pig)
+        
+        bandeirao.run(repeatBandeira, withKey: "repeatBandeira")
+        addChild(bandeirao)
         //
         //        pig2.run(group, withKey: "repeatAndando")
         //
@@ -132,7 +149,7 @@ class HomeScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         let sceneOptions = HomeOptionsScene(size: (self.scene?.size)!)
-         self.view?.presentScene(sceneOptions, transition: SKTransition.crossFade(withDuration: 1.3))
+        self.view?.presentScene(sceneOptions, transition: SKTransition.crossFade(withDuration: 1.3))
         //        view?.presentScene(sceneOptions)
     }
     
