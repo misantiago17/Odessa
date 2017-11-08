d73cb944c1531ea20ad9b9f9fae3846e4e6e2fda
marimend <marielaandrade@macbook-pro-de-mariela.local>
2017-11-08 16:28:02 -0200

    comenta

--- ParallaxScene.swift
+++ ParallaxScene.swift
@@ -23,23 +23,23 @@ class ParallaxScene: SKScene {
     
     override func sceneDidLoad() {
         
-        meio = SKSpriteNode(imageNamed: "mid")
-        meioNext = meio.copy() as! SKSpriteNode
-        
-        meio.position =  CGPoint(x: size.width / 2.0, y: size.height / 2.0)
-        meio.zPosition = -4
-
-        frente = SKSpriteNode(imageNamed: "front")
-        meioNext = frente.copy() as! SKSpriteNode
-        
-        meioNext.position = CGPoint(x: meio.position.x + meio.size.width, y: meio.position.y)
-        meioNext.zPosition = -4
-        
-        frente.position = CGPoint(x: size.width , y: size.height + 100)
-        frente.zPosition = -3
-        
-        frenteNext = frente.copy() as! SKSpriteNode
-        frenteNext.position = CGPoint(x: frente.position.x + frente.size.width, y: frente.position.y)
+//        meio = SKSpriteNode(imageNamed: "mid")
+//        meioNext = meio.copy() as! SKSpriteNode
+//        
+//        meio.position =  CGPoint(x: size.width / 2.0, y: size.height / 2.0)
+//        meio.zPosition = -4
+//
+//        frente = SKSpriteNode(imageNamed: "front")
+//        meioNext = frente.copy() as! SKSpriteNode
+//        
+//        meioNext.position = CGPoint(x: meio.position.x + meio.size.width, y: meio.position.y)
+//        meioNext.zPosition = -4
+//        
+//        frente.position = CGPoint(x: size.width , y: size.height + 100)
+//        frente.zPosition = -3
+//        
+//        frenteNext = frente.copy() as! SKSpriteNode
+//        frenteNext.position = CGPoint(x: frente.position.x + frente.size.width, y: frente.position.y)
 
     }
     
