c489b911e3f1bc2730ac870bb3300a4dcd6491fc
marimend <marielaandrade@MacBook-Pro-de-Mariela.local>
2017-11-16 18:07:43 -0200

    aaa

--- Odessa/GameScene.swift
+++ Odessa/GameScene.swift
@@ -37,7 +37,7 @@ class GameScene: SKScene,  SKPhysicsContactDelegate {
     var mapa: Mapa?
     var HUDNode = HUD()
     var movements = Movimentacao()
- //   var parallax = ParallaxView()
+//    var parallax = ParallaxScene()
     
     // Private
     private var modules: [SKSpriteNode] = []
@@ -682,7 +682,7 @@ class GameScene: SKScene,  SKPhysicsContactDelegate {
             inimigoNode.physicsBody?.usesPreciseCollisionDetection = true
             inimigoNode.physicsBody?.categoryBitMask = PhysicsCategory.enemy
             
-          //  inimigoNode.addChild(enemyHealthBar)
+//            inimigoNode.addChild(enemyHealthBar)
             
 //            let inimigoHealthBar = SKSpriteNode()
 //            updateHealthBar(node: inimigoHealthBar, withHealthPoints: enemyHP)
